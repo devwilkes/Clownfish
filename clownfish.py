d80d4e365e0c78a8b186a84c0acc72f5a600cb0e
Devon Wilkes <devonowilkes@hotmail.com>
2023-10-21 22:48:28 -0400

    Added docstrings
    We love docstrings!!!!!!!!

--- clownfish.py
+++ clownfish.py
@@ -16,21 +16,42 @@ async def on_ready():
 
 bot = commands.Bot(command_prefix='!')
 
-
 @bot.command(name='69')
 async def nice(ctx):
+    """ Automatically detects '69' in user messages and replies with 'nice'.
+    """
     response = 'nice'
     await ctx.send(response)
+    
+@bot.command(name="smack")
+async def smack(ctx):
+    """ Allows a user to smack another user across the face (virtually).
+
+    Args:
+        ctx (_type_): _description_
+    """
+    
 
 
 @bot.command(name="nice_egg")
 async def offer_egg(ctx):
+    """ Offers the user an image of an egg.
+    It's Always Sunny In Philadelphia reference.
+
+    Args:
+        ctx (_type_): _description_
+    """
     response = "Can I offer you a nice egg in this trying time?"
     await ctx.send(response)
 
 
 @bot.command(name="dad_joke")
 async def dad_joke(ctx):
+    """ Tells the user a corny dad joke.
+
+    Args:
+        ctx (_type_): _description_
+    """
     response = ""
 
 client.run(TOKEN)
